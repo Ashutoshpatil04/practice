b908c6072fb787614e1b86ceaaf753dba67f6a13
Ashutosh Patil <ashutoshpatil817@gmail.com>
2025-03-06 19:18:56 +0530

    Add initial code.py file

--- code.py
+++ code.py
@@ -39,8 +39,14 @@ print("Failed login attempts per user:" , failed_attempts)
 
 
  
+git add README.md
+echo "# practice" >> README.md
+git init
 
-
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ashutoshpatil04/practice.git
+git push -u origin main
 
 
 
